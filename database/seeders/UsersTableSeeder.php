08ad17f49f5846c08500ae1479631fd4b36a0df1
apluszure <apluszure@gmail.com>
2021-07-05 22:56:19 +0800

    initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'admin@onestep.com',
+            'password' => bcrypt('eoqkr123')
+        ]);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,84 @@
+
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+<link href="css/import.css" rel="stylesheet" type="text/css" />
+<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=10, minimum-scale=1.0">
+<title>스타포렉스 본사</title>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+<!-- Global site tag (gtag.js) - Google Ads: 452729914
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-452729914"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-452729914');
+</script>
+
+<!-- Event snippet for 기타 conversion page
+In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button.
+<script>
+function gtag_report_conversion(url) {
+  var callback = function () {
+   if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-452729914/TbkcCJHK_e4BELq48NcB',
+      'event_callback': callback
+  });
+  return false;
+}
+</script>
+-->
+
+</head><!--popup 소스-->
+
+
+<body>
+
+<script>
+    function fnMove(seq){
+        var offset = $("#content" + seq).offset();
+        $('html, body').animate({scrollTop : offset.top}, 400);
+    }
+</script>
+
+<div class="wrap">
+	<div class="topnav">
+		<div class="imgBox mb0"><a href="https://starforex01.com/ref/3261JUVCJUIzJUI4JUVDJTgyJUFD" target="_blank"><img src="images/btn07.png" alt="스타포렉스 상단 회원가입" /></a></div>
+	</div><!--@ end .topnav 끝 //-->
+
+    <div class="imgBox mb0" style="background:url(images/img_bg.jpg) no-repeat; background-size:cover;" id="content2">
+		<img src="images/img01.png" alt="텍스트" />
+		<a href="https://t.me/starforex777" target="_blank"><img src="images/btn02.png" alt=" 텔레그램" class="imgBox2 mb10" /></a>
+		<a href="https://open.kakao.com/o/s8q6Vr1c" target="_blank"><img src="images/btn01.png" alt="카카오톡" class="imgBox2 mb10" /></a>
+		<img src="images/img01-1.png" alt="텍스트" />
+		<img src="images/img03.jpg" alt="화살표" class="imgBox3 mb100"  onClick="fnMove('1')" />
+	</div>
+
+    <div class="imgBox mb0" id="content1"><img src="images/img04.jpg" alt="상세이미지" /></div>
+    <div class="imgBox mb0"><a href="https://starforex01.com/ref/3261JUVCJUIzJUI4JUVDJTgyJUFD" target="_blank"><img src="images/img05.jpg" alt="무료개설 상담문의" /></a></div>
+	<div class="imgBox mb0"><img src="images/img06.jpg" alt="상세이미지" /></div>
+
+    <div class="imgBox mb0">
+		<a href="https://starforex01.com/ref/3261JUVCJUIzJUI4JUVDJTgyJUFD" target="_blank"><img src="images/btn04.png" alt=" 회원가입" class="imgBox2 mb10" /></a>
+		<a href="https://t.me/starforex777" target="_blank"><img src="images/btn05.png" alt=" 텔레그램" class="imgBox2 mb10" /></a>
+		<a href="https://open.kakao.com/o/s8q6Vr1c" target="_blank"><img src="images/btn06.png" alt="카카오톡" class="imgBox2" /></a>
+	</div>
+
+	<div class="footer">
+		<div class="imgBox mb0" onClick="fnMove('2')"><img src="images/img07.jpg" alt="화살표" class="imgBox3" /></div>
+	</div>
+
+
+</div><!--@ end .wrap 끝 //-->
+<iframe name="VlineDiet" style="width:1px; height:1px; position:absolute; top:-100px;"></iframe>
+</body>
+</html>
